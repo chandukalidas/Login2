a4bc3025fce3b20a25cdc40b5c8c114c0c48857a
chandukalidas <chandu.kalidas2995@gmail.com>
2023-02-06 12:31:02 +0530

    new java script

--- demo.js
+++ demo.js
@@ -1,4 +1,9 @@
 var header=document.getElementById("main-header");
 header.style.borderBottom="solid 4px black";
-var items=document.getElementById("main");
-items.style.color="green";
+//var items=document.getElementById("main");
+//items[0].style.color="green";
+var list=document.getElementsByClassName("list-group-item");
+list[2].style.backgroundColor="green";
+for(var i=0;i<list.length;i++){
+    list[i].style.fontWeight="bold";
+}
